e6a6bff476182927cfc6aa35d84d037e69e5f10f
Divya R <kwrdivyaraj100@gmail.com>
2023-12-26 17:05:21 +0530

    Added Settings component with Sidenav

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,5 +12,5 @@ export default function About() {
                 </Box>
             </Box>
         </>
-    )
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,5 +41,5 @@ export default function Home() {
                 </Box>
             </Box>
         </>
-    )
+    );
 }
