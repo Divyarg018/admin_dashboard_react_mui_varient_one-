587c037df12dcfa81ca49670b8642240639456f8
Divya R <kwrdivyaraj100@gmail.com>
2023-12-26 17:23:03 +0530

    Added Appbar code from mui website in Navbar.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Sidenav from '../Sidenav';
+import Sidenav from '../components/Sidenav.jsx';
+import Navbar from '../components/Navbar.jsx';
 import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
 
 
 export default function Home() {
     return (
         <>
+        <Navbar/>
             <Box sx={{ display: 'flex' }}>
                 <Sidenav />
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
